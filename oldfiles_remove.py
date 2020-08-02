47aa0493da39a343c5b56a549c72512f1da29561
tarou640 <68978606+tarou640@users.noreply.github.com>
2020-08-02 20:09:19 +0900

    test commit

--- oldfiles_remove.py
+++ oldfiles_remove.py
@@ -62,7 +62,7 @@ def main():
         base, ext = os.path.splitext(file)
         if ext == '.csv':
             #filelists_csv.append([file, os.path.getctime(str(local_path_csv) + "/" + file)])
-            filelists_csv.append([file, str(base)[0:8])
+            filelists_csv.append([file, str(base)[0:10])
     filelists_csv.sort(key=itemgetter(1), reverse=True)
     for i,file in enumerate(filelists_csv):
         if i > MAX_CNT - 1:
