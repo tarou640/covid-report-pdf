1c53129aee275c85396aa988885438990cefce35
tarou640 <tarou_640@yahoo.co.jp>
2020-08-03 18:52:07 +0900

    test commit

--- runrm.sh
+++ runrm.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # 古いファイルを削除
-#./oldfiles_remove.py
-echo "不要ファイル削除（csv）"
+echo "## 不要ファイル削除処理 開始"
 
-TARGET_DIR=./csv
-#DELETE_DAY=`date "+%Y%m%d" --date '30 days ago'`
 DELETE_FILE_MAX=30
 
+
+# pdfファイル
+echo "## 削除処理（pdf）"
+
+TARGET_DIR=./pdf
 cd $TARGET_DIR
 FILE_LIST=`ls "-r"`
 
@@ -18,17 +20,6 @@ do
  if [[ ${FILE} =~ 202[0-9]{5} ]]
  then
 
-  # ファイル名日付部分を取り出す
-  #FILE_DATE=`expr "${FILE}" : ".*\(202[0-9]\{5\}\)"`
-
-  # 日付を比較して削除
-  #if [ ${FILE_DATE} -lt ${DELETE_DAY} ]
-  #then
-  # #rm ${FILE}
-  # #git rm ./csv/"${FILE}"
-  # echo "./csv/"${FILE}"は削除します"
-  #fi
-  
   DELETE_FILE_CNT=$(expr $DELETE_FILE_CNT + 1)
   if [ ${DELETE_FILE_CNT} -gt ${DELETE_FILE_MAX} ]
   then
@@ -37,3 +28,32 @@ do
   fi
  fi
 done
+echo "削除ファイル数（pdf）:"${DELETE_FILE_CNT}""
+
+
+# csvファイル
+echo "## 削除処理（csv）"
+
+TARGET_DIR=./csv
+cd $TARGET_DIR
+FILE_LIST=`ls "-r"`
+
+DELETE_FILE_CNT=0
+for FILE in $FILE_LIST
+do
+ # ファイル名にYYYYMMDDを含む
+ if [[ ${FILE} =~ 202[0-9]{5} ]]
+ then
+
+  DELETE_FILE_CNT=$(expr $DELETE_FILE_CNT + 1)
+  if [ ${DELETE_FILE_CNT} -gt ${DELETE_FILE_MAX} ]
+  then
+    git rm "${FILE}"
+    echo ""$TARGET_DIR"/"${FILE}"は削除します"
+  fi
+ fi
+done
+echo "削除ファイル数（csv）:"${DELETE_FILE_CNT}""
+
+
+echo "## 不要ファイル削除処理 終了"
