9e562784ba40464609ba7228d13a9804ee55cd1b
tarou640 <tarou_640@yahoo.co.jp>
2020-08-03 15:50:03 +0900

    test commit

--- runrmm.sh
+++ runrmm.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
+# 古いファイルを削除
+#./oldfiles_remove.py
 echo "不要ファイル削除（csv）"
 
-TARGET_DIR=/csv
-DELETE_DAY=`date +"%Y%m%d" -d '14 days ago'`
+TARGET_DIR=./csv
+#DELETE_DAY=`date "+%Y%m%d" --date '30 days ago'`
+DELETE_FILE_MAX=30
 
 cd $TARGET_DIR
-FILE_LIST=`ls`
+FILE_LIST=`ls "-r"`
 
+DELETE_FILE_CNT=0
 for FILE in $FILE_LIST
 do
  # ファイル名にYYYYMMDDを含む
- if [[ ${FILE} =~ 201[0-9]{5} ]]
+ if [[ ${FILE} =~ 202[0-9]{5} ]]
  then
 
   # ファイル名日付部分を取り出す
-  FILE_DATE=`expr "${FILE}" : ".*\(201[0-9]\{5\}\)"`
+  #FILE_DATE=`expr "${FILE}" : ".*\(202[0-9]\{5\}\)"`
 
   # 日付を比較して削除
-  if [ ${FILE_DATE} -lt ${DELETE_DAY} ]
+  #if [ ${FILE_DATE} -lt ${DELETE_DAY} ]
+  #then
+  # #rm ${FILE}
+  # #git rm ./csv/"${FILE}"
+  # echo "./csv/"${FILE}"は削除します"
+  #fi
+  
+  DELETE_FILE_CNT=$(expr $DELETE_FILE_CNT + 1)
+  if [ ${DELETE_FILE_CNT} -gt ${DELETE_FILE_MAX} ]
   then
-   #rm ${FILE}
-   echo ${FILE} "は削除します"
+    git rm "${FILE}"
+    echo ""$TARGET_DIR"/"${FILE}"は削除します"
   fi
  fi
 done
