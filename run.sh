707f8bfdfb601343289f8424059cbbe70fe24a69
tarou640 <68978606+tarou640@users.noreply.github.com>
2020-08-03 15:32:03 +0900

    test commit

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@ echo "不要ファイル削除（csv）"
 
 TARGET_DIR=./csv
 DELETE_DAY=`date "+%Y%m%d" --date '30 days ago'`
-DELETE_FILE_MAX = 30
+DELETE_FILE_MAX=30
 
 
 cd $TARGET_DIR
-FILE_LIST=`ls　-r`
+FILE_LIST=`ls "-r"`
 
-DELETE_FILE_CNT = 0
+DELETE_FILE_CNT=0
 for FILE in $FILE_LIST
 do
  # ファイル名にYYYYMMDDを含む
@@ -30,7 +30,7 @@ do
   # echo "./csv/"${FILE}"は削除します"
   #fi
   
-  DELETE_FILE_CNT = DELETE_FILE_CNT + 1
+  DELETE_FILE_CNT=$(expr $DELETE_FILE_CNT + 1)
   if [ ${DELETE_FILE_CNT} -gt ${DELETE_FILE_MAX} ]
   then
     echo "./csv/"${FILE}"は削除します"
