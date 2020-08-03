51c300ee9702db94305e4ae6dc6765c6b4515bd2
tarou640 <68978606+tarou640@users.noreply.github.com>
2020-08-03 14:59:03 +0900

    test commit

--- run.sh
+++ run.sh
@@ -6,10 +6,13 @@ echo "不要ファイル削除（csv）"
 
 TARGET_DIR=./csv
 DELETE_DAY=`date "+%Y%m%d" --date '30 days ago'`
+DELETE_FILE_MAX = 30
+
 
 cd $TARGET_DIR
-FILE_LIST=`ls`
+FILE_LIST=`ls　-r`
 
+DELETE_FILE_CNT = 0
 for FILE in $FILE_LIST
 do
  # ファイル名にYYYYMMDDを含む
@@ -17,14 +20,20 @@ do
  then
 
   # ファイル名日付部分を取り出す
-  FILE_DATE=`expr "${FILE}" : ".*\(202[0-9]\{5\}\)"`
+  #FILE_DATE=`expr "${FILE}" : ".*\(202[0-9]\{5\}\)"`
 
   # 日付を比較して削除
-  if [ ${FILE_DATE} -lt ${DELETE_DAY} ]
+  #if [ ${FILE_DATE} -lt ${DELETE_DAY} ]
+  #then
+  # #rm ${FILE}
+  # #git rm ./csv/"${FILE}"
+  # echo "./csv/"${FILE}"は削除します"
+  #fi
+  
+  DELETE_FILE_CNT = DELETE_FILE_CNT + 1
+  if [ ${DELETE_FILE_CNT} -gt ${DELETE_FILE_MAX} ]
   then
-   #rm ${FILE}
-   #git rm ./csv/"${FILE}"
-   echo "./csv/"${FILE}"は削除します"
+    echo "./csv/"${FILE}"は削除します"
   fi
  fi
 done
