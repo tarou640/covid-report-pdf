9023516cd3097dfbe9e3b6c7f49a149a020ffc8e
tarou640 <68978606+tarou640@users.noreply.github.com>
2020-08-03 14:20:18 +0900

    test commit

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@ echo "不要ファイル削除（csv）"
 DELETE_DAY=`date "+%Y%m%d" --date '14 days ago'`
 
 #cd $TARGET_DIR
-FILE_LIST=`ls "csv"`
+FILE_LIST=`ls './csv'`
 
 for FILE in $FILE_LIST
 do
  # ファイル名にYYYYMMDDを含む
- if [[ ${FILE} =~ 201[0-9]{5} ]]
+ if [[ ${FILE} =~ 202[0-9]{5} ]]
  then
 
   # ファイル名日付部分を取り出す
-  FILE_DATE=`expr "${FILE}" : ".*\(201[0-9]\{5\}\)"`
+  FILE_DATE=`expr "${FILE}" : ".*\(202[0-9]\{5\}\)"`
 
   # 日付を比較して削除
   if [ ${FILE_DATE} -lt ${DELETE_DAY} ]
