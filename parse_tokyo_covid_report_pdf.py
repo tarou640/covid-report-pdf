4085b330f5f45be012933d15edaffbfef0759714
tarou640 <tarou_640@yahoo.co.jp>
2020-08-04 20:51:26 +0900

    test commit

--- parse_tokyo_covid_report_pdf.py
+++ parse_tokyo_covid_report_pdf.py
@@ -48,7 +48,7 @@ def main():
         #lines[table_text.y1].extend(t_list)
         if len(str(t_list)) > 0:
             lines[table_text.y1].extend([i for i in t_list if '(' not in i and len(i) < 7])
-            logger.warning("lines:" + str([i for i in t_list if '(' not in i and len(i) < 7]))
+            #logger.warning("lines:" + str([i for i in t_list if '(' not in i and len(i) < 7]))
 
 
 
