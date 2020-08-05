0be446dfef77d3123c785eb1b6577051e6c91ff1
tarou640 <68978606+tarou640@users.noreply.github.com>
2020-08-05 20:11:00 +0900

    test commit

--- parse_tokyo_covid_report_pdf.py
+++ parse_tokyo_covid_report_pdf.py
@@ -46,11 +46,12 @@ def main():
         t_list = table_text.get_text().strip().split()  # たまに一つの LTTextLine に複数テキストがあるので split
         #t_list = table_text.get_text().split()
         #lines[table_text.y1].extend(t_list)
-        #if len(str(t_list)) > 0:
-            lines[table_text.y1].extend([i for i in t_list if '(' not in i and not len(i) and len(i) < 7])
+        if len(str(t_list)) > 0:
+            #lines[table_text.y1].extend([i for i in t_list if '(' not in i and not len(i) and len(i) < 7])
             #logger.warning("lines:" + str([i for i in t_list if '(' not in i and not len(i) and len(i) < 7]))
 
-
+            if len([i for i in t_list if '(' not in i and not len(i) and len(i) < 7]) > 0:
+                lines[table_text.y1].extend([i for i in t_list if '(' not in i and not len(i) and len(i) < 7])
 
     # 各行で対応する要素を出力
     for k1, k2 in pairs(lines):
