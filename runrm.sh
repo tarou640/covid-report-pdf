2d2940583e326cc29bacf0de077126875603e0e3
tarou640 <tarou_640@yahoo.co.jp>
2020-12-05 22:20:16 +0900

    年齢別感染者情報ファイルの定期削除対応

--- runrm.sh
+++ runrm.sh
@@ -60,4 +60,31 @@ done
 echo "削除ファイル数（csv）:"${DELETE_FILE_CNT}""
 
 
+# cssファイル
+echo "## 削除処理（css）"
+
+TARGET_DIR=../css
+cd $TARGET_DIR
+FILE_LIST=`ls "-r"`
+
+FILE_CNT=0
+DELETE_FILE_CNT=0
+for FILE in $FILE_LIST
+do
+ # ファイル名にYYYYMMDDを含む
+ if [[ ${FILE} =~ 202[0-9]{5} ]]
+ then
+
+  FILE_CNT=$(expr $FILE_CNT + 1)
+  if [ ${FILE_CNT} -gt ${DELETE_FILE_MAX} ]
+  then
+    git rm "${FILE}"
+    #echo ""$TARGET_DIR"/"${FILE}"は削除します"
+    DELETE_FILE_CNT=$(expr $DELETE_FILE_CNT + 1)
+  fi
+ fi
+done
+echo "削除ファイル数（css）:"${DELETE_FILE_CNT}""
+
+
 echo "## 不要ファイル削除処理 終了"
