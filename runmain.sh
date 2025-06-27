3dab07caa6ae0d2068e064bbd4d00cb7f01e2687
tarou640 <tarou_640@yahoo.co.jp>
2025-06-27 19:06:19 +0900

    test

--- runmain.sh
+++ runmain.sh
@@ -4,7 +4,7 @@ TARGET_URL="https://survey.tmiph.metro.tokyo.lg.jp/epidinfo/weeklyhc.do"
 TEMP_FILE="temp.dat"
 
 # 更新日付 をとってくる
-NEW_DAT_FILE=$(./parse_tokyo_covid_report_update.py ${TARGET_URL})
+NEW_DAT_FILE=$(./parse_tokyo_covid_report_updt.py ${TARGET_URL})
 if [ -z "$NEW_DAT_FILE" ]; then
   echo "エラー: NEW_DAT_FILE が未設定または空です。"
   exit 255
@@ -15,7 +15,7 @@ if [ -e "$NEW_DAT_FILE" ]; then
 fi
 
 # 都の感染者情報から市区町村別の感染者情報を抜粋する
-./parse_tokyo_covid_report.py ${TARGET_URL} > ${NEW_DAT_FILE}
+./parse_tokyo_covid_report_main.py ${TARGET_URL} > ${NEW_DAT_FILE}
 if [ ! -f "$NEW_DAT_FILE" ]; then
   echo "エラー: ファイルが存在しません: $NEW_DAT_FILE"
   exit 255
